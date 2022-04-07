32e8f616c17a44b75fd7f518321ea6725261f84e
saretotracy <tracysareto@gmail.com>
2022-04-07 21:30:26 +0300

    connect database to settings

--- project_review/settings.py
+++ project_review/settings.py
@@ -75,13 +75,15 @@ WSGI_APPLICATION = 'project_review.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'review',
+        'USER': 'moringa',
+        'PASSWORD':'kimachas',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
