fdd9c8110249e0c40cac7fe3d2bee3eb01f88c47
saretotracy <tracysareto@gmail.com>
2022-04-08 20:45:27 +0300

    create an API view to handle the requests.

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  MoringaMerch
+from .serializer import MerchSerializer
 
 # Create your views here.
 def index(request):  
@@ -14,3 +18,9 @@ def projects(request):
     
     
     return render(request, 'projects.html')
+
+class ProfileList(APIView):
+   def get(self, request, format=None):
+       all_Profile = Profile.objects.all()
+       serializers = ProfileSerializer(all_Profile, many=True)
+       return Response(serializers.data)
