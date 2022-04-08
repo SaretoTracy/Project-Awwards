27730fc2b9c80227fb6b6ae003fc6d0d0d832e60
saretotracy <tracysareto@gmail.com>
2022-04-08 20:37:20 +0300

    add model class

--- project_review/settings.py
+++ project_review/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects.apps.ProjectsConfig',
-    'bootstrap5'
-    'cloudinary'
+    'bootstrap5',
+    'cloudinary',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+import datetime as dt
 # Create your models here.
+
+class profile  (models.Model):
+    bio = models.CharField(max_length=250)
+    image = CloudinaryField('image/', default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    def __str__(self):
+        return self.bio
+    
+    def save_projects(self):
+        self.save()
+    def delete_projects(self):
+        self.delete()
+
+class Projects (models.Model):
+    title = models.CharField(max_length=60)
+    image = CloudinaryField('image/', default="")
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    link = models.CharField(max_length=60)
+    def __str__(self):
+        return self.title
+    
+    def save_projects(self):
+        self.save()
+    def delete_projects(self):
+        self.delete()
