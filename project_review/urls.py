9cf79aa8daa750bac97fd995da5b833b660a4a63
saretotracy <tracysareto@gmail.com>
2022-04-10 23:09:48 +0300

    ageneral page styling

--- project_review/urls.py
+++ project_review/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from projects import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,3 +38,8 @@ urlpatterns = [
     
 
 ]
+
+
+#serve uploaded images on the development server 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
